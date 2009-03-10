2d12ea40737f7f602b9c9ce41ea907d8ca1bb58f
Vincent Dubois <dubois.vct@free.fr>
2009-03-10 19:30:11 +0100

    Using Open3::popen3 in tests builder for rake tasks

--- lib/tests_formatter.rb
+++ lib/tests_formatter.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'XmlElements'
+require 'cgi'
 # =====================================================
 # Classe de formatage des resultats renvoyes par les
 # differents tests unitaires
@@ -14,10 +15,29 @@ class TestsFormatter
     project = XmlElements.fromString(File.read("continuous4r-project.xml"))
     errors_or_warnings = 0
     html_details = ""
+    if Config::CONFIG['host_os'] =~ /mswin/
+      require 'win32/open3'
+    else
+      require 'open3'
+    end
     ['units', 'functionals', 'integration'].each do |runner|
       puts " Running #{runner} tests..."
-      result = Utils.run_command("rake test:#{runner}")
-      passed = (result.index("Failure:").nil? and result.index("Error:").nil? and result.index("pending migrations").nil? and result.split(/$/).length > 1)
+      error_detail = ""
+      result = ""
+      passed = false
+      Open3::popen3("rake test:#{runner}") do |stdin, stdout, stderr, pid|
+        result = stdout.read.strip
+        if result.match(/Finished in/).nil?
+          error_detail = stderr.read.strip
+        end
+      end
+      passed = (result.index("Failure:").nil? and result.index("Error:").nil? and error_detail.match(/rake aborted/).nil?)
+      if !(error_detail.match(/rake aborted/).nil?) and error_detail.split(/$/).length > 1
+        arr_error = error_detail.split(/$/)
+        arr_error.delete_at(arr_error.length - 1)
+        arr_error.delete_at(0)
+        error_detail = arr_error.to_s
+      end
       html += "<tr class='#{ i % 2 == 0 ? 'a' : 'b'}' style='#{passed == true ? "background-color: #e3ffdb; color: #7ab86c;" : "background-color: #ffdddd; color: #770000;"}'><td><strong>#{runner}</strong></td>"
       if project['ignore-tests-failures'] == "false" and passed == false
         raise " #{runner} tests failed.\n BUILD FAILED."
@@ -28,7 +48,7 @@ class TestsFormatter
       html += "<td style='text-align: center;'><img src='images/icon_#{passed ? 'success' : 'error'}_sml.gif'/></td>"
       file_content = File.read("#{Continuous4r::WORK_DIR}/test_#{runner}.log")
       array_file_content = file_content.split(/$/)
-      test_results = array_file_content[array_file_content.length - 2].split(/, /)
+      test_results = array_file_content[array_file_content.length - 1].split(/, /)
       tests = test_results[0]
       assertions = test_results[1]
       failures = test_results[2]
@@ -42,24 +62,25 @@ class TestsFormatter
         array_get = false
         index = 0
         array_file_content.each do |line|
-          array_get = !array_get if line.length == 1
+          index += 1 if array_get and line.match(Regexp.new("#{index + 2}\\)"))
+          array_get = true if line.match(Regexp.new("#{index + 1}\\)"))
           if array_get and line.match(/assertions/).nil?
             array_details[index] = Array.new if array_details[index].nil?
-            next if array_details[index].length == 0 and line.match(/^ /).nil?
+            next if array_details[index].length == 0 and line.match(/[0-9]\)/).nil?
             array_details[index] << line
           end
-          index += 1 if array_get and line.length == 0
         end
-        #puts "#{index} : #{array_details[index][0]}"
-        error_failure_type = array_details[index][0].split(/\)/)[1].split(/:/)[0]
-        error_icon = (error_failure_type.match(/Error/) ? "<img src='images/exclamation.png'/>" : "<img src='images/error.png'/>")
-        html_details += "<tr style='#{error_failure_type.match(/Error/) ? "background-color: #ffdddd; color: #770000;" : "background-color: #fffccf; color: #666600;"}'><td align='center'>#{error_icon}</td><td>#{array_details[index][1].split(/\(/)[1].split(/\)/)[0]}##{array_details[index][1].split(/\(/)[0]}</td>"
-        array_details[index].delete_at(1)
-        array_details[index].delete_at(0)
-        html_details += "<td><pre>#{array_details[index]}</pre></td></tr>"
+        (0..index).to_a.each do |arr_index|
+          error_failure_type = array_details[arr_index][0].split(/\)/)[1].split(/:/)[0]
+          error_icon = (error_failure_type.match(/Error/) ? "<img src='images/exclamation.png'/>" : "<img src='images/error.png'/>")
+          html_details += "<tr style='#{error_failure_type.match(/Error/) ? "background-color: #ffdddd; color: #770000;" : "background-color: #fffccf; color: #666600;"}'><td align='center'>#{error_icon}</td><td><strong>#{array_details[arr_index][1].split(/\(/)[1].split(/\)/)[0]}##{array_details[arr_index][1].split(/\(/)[0]}</strong>"
+          array_details[arr_index].delete_at(1)
+          array_details[arr_index].delete_at(0)
+          html_details += "<br/><pre>#{CGI::escapeHTML(array_details[arr_index].to_s)}</pre></td></tr>"
+        end
       end
       if array_file_content.select{|l| l =~ /^Finished in/}.length == 0
-        html += "<td>0</td><td>&#160;</td><td>0 seconds</td></tr>"
+        html += "<td>0</td><td>#{error_detail if !passed}</td><td>0 seconds</td></tr>"
       else
         html += "<td>#{tests.split(/ tests/)[0]}</td><td><img src='images/accept.png' align='absmiddle'/>&#160;#{assertions}&#160;&#160;<img src='images/error.png' align='absmiddle'/>&#160;#{failures}&#160;&#160;<img src='images/exclamation.png' align='absmiddle'/>&#160;#{errors}</td>"
         html += "<td>#{array_file_content.select{|l| l =~ /^Finished in/}[0].split(/Finished in /)[1].split(/\.$/)[0]}</td></tr>"
@@ -68,6 +89,6 @@ class TestsFormatter
     end
     html += "</tbody></table>"
     return html if errors_or_warnings == 0
-    html += "<h3>Errors/Failures details</h3><table class='bodyTable'><thead><th>Type</th><th>Method</th><th>Trace</th></thead>#{html_details}</table>"
+    html += "<h3>Errors/Failures details</h3><table class='bodyTable'><thead><th>Type</th><th>Method/Trace</th></thead>#{html_details}</table>"
   end
 end
