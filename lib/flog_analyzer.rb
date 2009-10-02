9585f732ef860a28be7dfb5ce6fc1cf3616882ca
Vincent Dubois <vdubois@sqli-1610-2.(none)>
2009-10-02 14:45:27 +0200

    adding analyzers

--- lib/flog_analyzer.rb
+++ lib/flog_analyzer.rb
@@ -10,16 +10,45 @@ class FlogAnalyzer
     @file = file
     @average = 0
     @total = 0
+    @flogged_methods = []
   end
 
   # traitement sur fichier a analyser
   def perform
-    File.open("flog.log", "wb+") do |file|
-      file.write("@file\n")
-    end
     Utils.run_command("flog #{@file} >> flog.log")
+    flog_extracted_lines = File.read(@file).split(/$/)
+    flog_extracted_lines.each do |line|
+      extract_content(line)
+    end
   end
 
-  def 
+  # getting average score
+  def average_score
+    @average
+  end
+
+  # getting total score
+  def total_score
+    @total
+  end
+
+  # getting flogged_methods
+  def flogged_methods
+    @flogged_methods
+  end
+
+  private
+  
+  # extracting content logic from flog log file 
+  def extract_content(str)
+    str_splitted = str.split(/: /)
+    if str_splitted.include?("flog total")
+      @total = str_splitted[0]
+    else if str_splitted.include?("flog/method average")
+      @average = str_splitted[0]
+    else
+      extract_method_content()
+    end
+  end
 end
 
